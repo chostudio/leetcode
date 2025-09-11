05ac03ed9c0a1821a0fba08d834a88c30b377afd
Chris Ho <68166971+chostudio@users.noreply.github.com>
2025-09-11 12:31:55 -1000

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -0,0 +1,30 @@
+// Last updated: 9/11/2025, 12:31:55 PM
+class Solution {
+    public List<String> fizzBuzz(int n) {
+            
+        List<String> ans = new ArrayList<String>();
+        
+        for(int i = 1; i <= n; i++){
+               
+            if(i % 3 == 0 && i % 5 != 0){
+                ans.add("Fizz");
+            }
+            
+                              
+            else if(i % 5 == 0 && i % 3 != 0){
+                ans.add("Buzz");
+            }
+               
+                              
+            else if(i % 3 == 0 && i % 5 == 0){
+               ans.add("FizzBuzz");
+            }
+            
+            else{
+                ans.add(Integer.toString(i));
+            }
+        }
+        
+        return ans;
+    }
+}
