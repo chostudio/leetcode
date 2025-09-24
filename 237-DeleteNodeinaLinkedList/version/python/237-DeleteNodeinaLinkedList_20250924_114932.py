d99cfd2d741b6705654727465480f9f96c989930
Chris Ho <68166971+chostudio@users.noreply.github.com>
2025-09-24 11:49:33 -0700

    Create 237-DeleteNodeinaLinkedList

---
+++ 237-DeleteNodeinaLinkedList/version/python/237-DeleteNodeinaLinkedList_20250924_114932.py
@@ -0,0 +1,17 @@
+# Last updated: 9/24/2025, 11:49:32 AM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        # and then remove next node
+        node.next = node.next.next
+        
